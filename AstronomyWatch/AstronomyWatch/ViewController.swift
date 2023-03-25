7ebcb5f5df0cb4b1a3bf7d9b65890691136f9a2d
Sathish Kumar S <sathish141994@gmail.com>
2023-03-25 21:27:31 +0530

    Initial commit - Project files

---
+++ AstronomyWatch/AstronomyWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstronomyWatch
+//
+//  Created by Sathish Kumar S on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
